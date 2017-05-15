1c99e249f6422c245a794b93594631a37760cd0f
tbscanlon <thomas.scanlon@outlook.com>
2017-05-15 19:06:22 +0100

    Complete session 1

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,14 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  a = string.split("")
+  return_string = Array.new
+
+  a.length.times { |x|     return_string.push(a[x + 1]) if a[x] == "r" || a[x] == "R" }
+
+  return_string.join("")
 end
