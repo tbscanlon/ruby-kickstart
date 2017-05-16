0c421eb347294d774ae4f7ab4ace3e173528856c
tbscanlon <thomas.scanlon@outlook.com>
2017-05-16 20:36:11 +0100

    Complete session 2 challenge 8

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,20 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(sentence)
+  # sentence = sentence.delete('!@$#%^&*()-=_+[]:;,./<>?\\|').split(" ")
+  return_a = Array.new
+
+  '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each {|regex| sentence = sentence.gsub(regex, ' ')}
+
+  a = sentence.split(" ")
+
+  a.each_index {|x| return_a.push a[x] if x.even?}
+  return_a
+end
--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -1,7 +1,12 @@
 # Given an array of elements, return true if any element shows up three times in a row
-# 
+#
 # Examples:
 # got_three? [1, 2, 2, 2, 3]  # => true
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(list)
+  list.each_index { |index| return true if list[index+1] == list[index] && list[index+2] == list[index] }
+  false
+end
