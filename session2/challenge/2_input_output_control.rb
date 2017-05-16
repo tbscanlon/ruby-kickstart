89a5f3eb4f04e2b3cc2ad7b483ee52f7ec86859d
tbscanlon <thomas.scanlon@outlook.com>
2017-05-16 14:16:46 +0100

    Complete session 2 challenge 2

--- session2/challenge/2_input_output_control.rb
+++ session2/challenge/2_input_output_control.rb
@@ -1,5 +1,5 @@
 # Prompt the user for a number, then read it in and print out "hi" that many times
-# 
+#
 # Repeat this process until the user submits "bye", then say "goodbye" and end the program
 # HINT: Check out example 2 if you get stuck
 
@@ -20,12 +20,22 @@
 
 def hi_hi_goodbye
   # your code here
+  puts "Hello. Enter a number:"
+
+  while user_input = gets
+    break if user_input.chomp == "bye"
+
+    user_input.chomp.to_i.times { |x| print "hi "}
+    puts "\nEnter a number (or bye to quit):"
+  end
+  
+  puts "goodbye"
 end
 
 
 
 
 # This will just invoke the method if you run this program directly
-# This way you can try it out by running "$ ruby 2_input_output_control.rb" 
+# This way you can try it out by running "$ ruby 2_input_output_control.rb"
 # but it will still work for our tests
 hi_hi_goodbye if $0 == __FILE__
